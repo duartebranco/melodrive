aa0adce7228b46e2e10052ab339bfef5286a6919
duartebranco <duartebranco35@gmail.com>
2026-03-30 13:52:12 +0100

    init NewPipe in Application.onCreate

--- app/src/main/java/com/melodrive/MeloDriveApp.kt
+++ app/src/main/java/com/melodrive/MeloDriveApp.kt
@@ -1,5 +1,13 @@
 package com.melodrive
 
 import android.app.Application
+import com.melodrive.youtube.NewPipeDownloader
+import org.schabi.newpipe.extractor.NewPipe
 
-class MeloDriveApp : Application()
+class MeloDriveApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        NewPipe.init(NewPipeDownloader())
+    }
+}
